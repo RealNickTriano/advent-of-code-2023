40f4e26c1b9bb0ff73b45b354fcc2fda852b8549
RealNickTriano <nicholastriano1999@gmail.com>
2023-12-13 11:58:08 -0500

    Day 13 Part 1 Point of Incidence; P2 not clear

--- src/main/java/org/aoc/day12/HotSprings.java
+++ src/main/java/org/aoc/day12/HotSprings.java
@@ -4,12 +4,16 @@ import org.aoc.utils.ReadFiles;
 
 import java.io.FileNotFoundException;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import static java.util.Arrays.asList;
 
 public class HotSprings {
     public static final String INPUT_FILE = "src/main/resources/inputs/day12.txt";
+    // HashMap to memoize results of ways function
+    public static Map<String, Long> memo = new HashMap<>();
     public static void main(String[] args) {
         String data = "";
         try {
@@ -18,11 +22,11 @@ public class HotSprings {
             e.printStackTrace();
         }
 
-        System.out.println(partOne(data));
-        //System.out.println(partTwo(data));
+        //System.out.println(partOne(data));
+        System.out.println(partTwo(data));
     }
 
-    private static int partOne(String data) {
+    private static long partOne(String data) {
         List<List<String>> lines = asList(data.split(System.lineSeparator())).stream()
                 .map(item -> List.of(item.split(" ")))
                 .toList();
@@ -38,11 +42,126 @@ public class HotSprings {
         //System.out.println(records);
         //System.out.println(requirements);
 
-        int result = 0;
+        long result = 0;
         for (int i = 0; i < records.size(); i++) {
-            result += printCombinations(records.get(i), requirements.get(i));
+            long num = ways(records.get(i), requirements.get(i));
+            System.out.println(num);
+            result += num;
         }
         return result;
+    }
+
+    private static long partTwo(String data) {
+        List<List<String>> lines = asList(data.split(System.lineSeparator())).stream()
+                .map(item -> List.of(item.split(" ")))
+                .toList();
+        //System.out.println(lines);
+
+        List<String> records = lines.stream()
+                .map(item -> item.get(0)
+                        + "?" + item.get(0)
+                        + "?" + item.get(0)
+                        + "?" + item.get(0)
+                        + "?" + item.get(0))
+                .toList();
+        List<List<Integer>> requirements = lines.stream()
+                .map(item -> Arrays.stream(new String(item.get(1)
+                        + "," + item.get(1)
+                        + "," + item.get(1)
+                        + "," + item.get(1)
+                        + "," + item.get(1))
+                        .split(",")).map(Integer::parseInt).toList())
+                .toList();
+
+        System.out.println(records);
+        System.out.println(requirements);
+
+        List<Integer> list = Arrays.asList(5, 22, 73, 2, -23);
+        list.sort(null);
+
+        long result = 0;
+        for (int i = 0; i < records.size(); i++) {
+            long num = ways(records.get(i), requirements.get(i));
+            //System.out.println(num);
+            result += num;
+        }
+        return result;
+    }
+
+    public static long ways(String record, List<Integer> requirements) {
+
+        if (record.isEmpty()) {
+            if (requirements.isEmpty()) {
+                //System.out.println("Match!");
+                return 1;
+            }
+            return 0;
+        }
+
+        if (requirements.isEmpty()) {
+            if (record.contains("#")) return 0;
+            //System.out.println("Match!");
+            return 1;
+        }
+
+        String key = record + " " + requirements;
+        if (memo.containsKey(key)) return memo.get(key);
+
+        long result = 0;
+
+        if (record.charAt(0) == '.' || record.charAt(0) == '?') {
+            //System.out.println(record + "\t" + requirements);
+            result += ways(record.substring(1), requirements);
+        }
+
+        if (record.charAt(0) == '#' || record.charAt(0) == '?') {
+                // there should be enough chars left in the string to
+                // satisfy the current block size condition
+            if (requirements.get(0) <= record.length()
+                    // The next number of characters in the string should not
+                    // contain a "." because that would interrupt the block
+                    && !record.substring(0, requirements.get(0)).contains(".")
+                    && (requirements.get(0) == record.length()
+                        || record.charAt(requirements.get(0)) != '#')) {
+
+                //System.out.println(record + "\t" + requirements);
+                //System.out.println("Block Consumed: " + record);
+                int startIndex = requirements.get(0) == record.length()
+                        ? requirements.get(0)
+                        : requirements.get(0) + 1;
+
+                result += ways(record.substring(startIndex)
+                        , requirements.subList(1, requirements.size()));
+
+            }
+        }
+
+        memo.put(key, result);
+        return result;
+    }
+
+    public static int printCombinations2(String record, List<Integer> requirement) {
+
+        for (int i = 0; i < record.length(); i++) {
+            String subRecord = record.substring(0, i + 1);
+            for (int j = 0; j < requirement.size(); j++) {
+                List<Integer> subRequirement = requirement.subList(0, j + 1);
+                for (int k = 0; k <= subRequirement.getLast(); k++) {
+
+                }
+            }
+        }
+        int matches = 0;
+        System.out.println(record);
+        if (!record.contains("?")) {
+            if (satisfiesRequirements(record, requirement))
+                return 1;
+            else return 0;
+        }
+        matches += printCombinations(record.replaceFirst("[?]", "#"), requirement);
+        matches += printCombinations(record.replaceFirst("[?]", "."), requirement);
+
+        return matches;
     }
 
     public static int printCombinations(String record, List<Integer> requirement) {
---
+++ src/main/java/org/aoc/day13/PointOfIncidence.java
@@ -0,0 +1,185 @@
+package org.aoc.day13;
+
+import org.aoc.utils.ReadFiles;
+
+import java.io.FileNotFoundException;
+import java.util.*;
+
+public class PointOfIncidence {
+    public static final String INPUT_FILE = "src/main/resources/inputs/day13.txt";
+    public static void main(String[] args) {
+        String data = "";
+        try {
+            data = ReadFiles.readFileToString(INPUT_FILE);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        //System.out.println(partOne(data));
+        System.out.println(partTwo(data));
+    }
+
+    private static long partOne(String data) {
+        List<String> lines = List.of(data.split(System.lineSeparator()));
+        List<List<List<String>>> matrices = new ArrayList<>();
+
+        List<List<String>> matrix = new ArrayList<>();
+        lines = lines.subList(1, lines.size());
+        for (String s : lines) {
+            if (s.isEmpty()) {
+                matrices.add(matrix);
+                matrix = new ArrayList<>();
+            } else{
+                matrix.add(List.of(s.split("")));
+            }
+        }
+        matrices.add(matrix);
+
+        long result = 0;
+
+        for (List<List<String>> m : matrices) {
+            for (List<String> r : m) {
+                System.out.println(r);
+            }
+            List<List<String>> transposed = transpose(m);
+            System.out.println("Transposed");
+            for (List<String> t : transposed) {
+                System.out.println(t);
+            }
+            result += findHorizontalLinesOfReflection(m) * 100;
+            result += findHorizontalLinesOfReflection(transposed);
+
+            System.out.println();
+        }
+
+        return result;
+    }
+
+    private static long partTwo(String data) {
+        List<String> lines = List.of(data.split(System.lineSeparator()));
+        List<List<List<String>>> matrices = new ArrayList<>();
+
+        List<List<String>> matrix = new ArrayList<>();
+        lines = lines.subList(1, lines.size());
+        for (String s : lines) {
+            if (s.isEmpty()) {
+                matrices.add(matrix);
+                matrix = new ArrayList<>();
+            } else{
+                matrix.add(Arrays.asList(s.split("")));
+            }
+        }
+        matrices.add(matrix);
+
+        long result = 0;
+
+        for (List<List<String>> m : matrices) {
+            for (List<String> r : m) {
+                System.out.println(r);
+            }
+
+            List<List<String>> transposed = transpose(m);
+            System.out.println("Transposed");
+            for (List<String> t : transposed) {
+                System.out.println(t);
+            }
+            long preRepair = findHorizontalLinesOfReflection(m);
+            long preRepairTransposed = findHorizontalLinesOfReflection(transpose(m));
+
+            m = checkAndRepairSmudges(m);
+
+            System.out.println("Fixed: ");
+            for (List<String> r : m) {
+                System.out.println(r);
+            }
+
+            long postRepair = findHorizontalLinesOfReflection(m);
+            long postRepairTransposed = findHorizontalLinesOfReflection(transpose(m));
+            System.out.println("Post Repair: " + postRepair);
+            if (preRepair != postRepair) {
+                result += (postRepair * 100) + postRepairTransposed;
+            } else {
+                result += (preRepair * 100) + preRepairTransposed;
+            }
+
+            System.out.println();
+        }
+
+        return result;
+    }
+
+    private static List<List<String>> checkAndRepairSmudges(List<List<String>> m) {
+        for (int i = 0; i < m.size(); i++) {
+            for (int j = i + 1; j < m.size(); j++) {
+                int index = compareLists(m.get(i), m.get(j));
+                if (index != -1) {
+                    // found smudge
+                    System.out.println("smudge: " + m.get(i) + " " + index);
+                    m.get(i).set(index, m.get(j).get(index));
+                    return m;
+                }
+            }
+        }
+
+        return m;
+    }
+
+    private static int compareLists(List<String> a, List<String> b) {
+        boolean oneDiff = false;
+        int index = 0;
+        for (int i = 0; i < a.size(); i++) {
+            if (!a.get(i).equals(b.get(i))) {
+                if (oneDiff) {
+                    return -1;
+                } else {
+                    oneDiff = true;
+                    index = i;
+                }
+            }
+        }
+        if (oneDiff) return index;
+
+        return -1;
+    }
+
+    public static long findHorizontalLinesOfReflection(List<List<String>> matrix) {
+        long result = 0;
+        for (int i = 0; i < matrix.size() - 1; i++) {
+            int rowsAbove = 0;
+            if (matrix.get(i).equals(matrix.get(i + 1))) {
+                System.out.println(matrix.get(i) + "\nEQUALS\n"  + matrix.get(i + 1));
+                int j = i + 2;
+                int k = i - 1;
+                boolean isReflection = true;
+                while(j < matrix.size() && k >= 0) {
+                    if (!matrix.get(k).equals(matrix.get(j))) {
+                        isReflection = false;
+                        break;
+                    }
+                    j++;
+                    k--;
+                }
+                if (isReflection) {
+                    // Count num rows above line of reflection
+                    rowsAbove = i + 1;
+                    return rowsAbove;
+                }
+            }
+            result += rowsAbove;
+        }
+        System.out.println(result);
+        return result;
+    }
+
+    public static List<List<String>> transpose(List<List<String>> A) {
+        List<List<String>> t = new ArrayList<>();
+        for (int col = 0; col < A.get(0).size(); col++) {
+            List<String> newRow = new ArrayList<>();
+            for (int row = 0; row < A.size(); row++) {
+                newRow.add(A.get(row).get(col));
+            }
+            t.add(newRow);
+        }
+        return t;
+    }
+}
